4a1f66627db317fc762372a54324770a2a24643b
mariasolegallina <gallinamariasole@gmail.com>
2024-04-23 09:15:00 +0200

    add - Project migration and seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,15 @@ class ProjectSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 15; $i++) {
+            $new_project = new Project();
+
+            $new_project->title = $faker->sentence(6);
+            $new_project->description = $faker->text(200);
+
+            $new_project->save();
+        }
     }
 }
